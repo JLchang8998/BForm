0616a0a708809321cd065fd9f264101bccd95d6f
JLchang8998 <92504740+JLchang8998@users.noreply.github.com>
2022-12-31 16:04:20 +0800

    Add files via upload

---
+++ loginform/bform/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class bform(models.Model):
+    form_pk = models.CharField(max_length=100000000,primary_key=True)
+    lineuid = models.CharField(max_length=50)
+    name = models.CharField(max_length=20, blank=False)
+    nickname = models.CharField(max_length=20, blank=False)
+    phone_number = models.CharField(max_length=10, blank=False)
+    email = models.CharField(max_length=100, blank=False)
+    address = models.CharField(max_length=50, blank=False)
